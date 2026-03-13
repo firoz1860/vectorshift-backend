e21ddf8d5e856a30bc00274f84e840e302cb26e4
Firoz <firozahmed709p@gmail.com>
2026-03-14 00:17:51 +0530

    fix cors for production

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://*.vercel.app",
+        "https://vectorshift-frontend-one.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
